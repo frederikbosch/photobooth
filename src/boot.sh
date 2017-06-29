f40ba77b42fa0d1fa5322794c051e3ed24029b21
Frederik Bosch <f.bosch@genkgo.nl>
2017-06-29 13:14:27 +0200

    usbreset

--- src/boot.sh
+++ src/boot.sh
@@ -1,4 +1,3 @@
-HITI_HD="yxz"
 /media/$HITI_HD/P51x_Linux_Driver_1.0.0/Install.sh
 add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) main universe restricted multiverse"
 apt-get install -y openssh-server
