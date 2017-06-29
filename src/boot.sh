e64581a5423d28aa9781aca1f5f60f0a331c1673
Frederik Bosch <f.bosch@genkgo.nl>
2017-06-29 17:26:11 +0200

    boot

--- src/boot.sh
+++ src/boot.sh
@@ -5,9 +5,10 @@ apt-get install -y openssh-server
 rm /etc/ssh/sshd_config
 rm /etc/init.d/ssh
 cp /media/dc7aa23f-297e-43c3-a1f4-64765c52c43c/home/sshd_config /etc/ssh/sshd_config
-cp /media/dc7aa23f-297e-43c3-a1f4-64765c52c43c/home/ssh.inid /etc/init.d/ssh
+cp /media/dc7aa23f-297e-43c3-a1f4-64765c52c43c/home/ssh.initd /etc/init.d/ssh
 /etc/init.d/ssh start
 cp /media/dc7aa23f-297e-43c3-a1f4-64765c52c43c/home/print.sh /root/print.sh
+mkdir /root/.ssh
 cat /media/dc7aa23f-297e-43c3-a1f4-64765c52c43c/home/laptop.key >> /root/.ssh/authorized_keys
 wget https://github.com/frederikbosch/photobooth/blob/master/src/print.sh -O /root/print.sh
 chmod +x /root/print.sh
