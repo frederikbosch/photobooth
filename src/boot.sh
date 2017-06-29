8162c8dd750a8412386620c0d3d909fb80dcfab7
Frederik Bosch <f.bosch@genkgo.nl>
2017-06-29 13:34:26 +0200

    sshd config

--- src/boot.sh
+++ src/boot.sh
@@ -4,4 +4,4 @@ apt-get update
 apt-get install -y openssh-server
 rm /etc/ssh/sshd_config
 cp /media/dc7aa23f-297e-43c3-a1f4-64765c52c43c/sshd_config /etc/ssh/sshd_config
-service restart openssh
+service restart ssh
