88d6c836ee5b06b2201c0f43641629509f87dec6
Frederik Bosch <f.bosch@genkgo.nl>
2017-06-29 13:26:28 +0200

    hd location

--- src/boot.sh
+++ src/boot.sh
@@ -1,4 +1,4 @@
-/media/$HITI_HD/P51x_Linux_Driver_1.0.0/Install.sh
+/media/dc7aa23f-297e-43c3-a1f4-64765c52c43c/P51x_Linux_Driver_1.0.0/Install.sh
 add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) main universe restricted multiverse"
 apt-get update
 apt-get install -y openssh-server
--- src/print.sh
+++ src/print.sh
@@ -1,7 +1,7 @@
-HITI_HD="yxz"
+HITI_HD="/media/dc7aa23f-297e-43c3-a1f4-64765c52c43c"
 HITI_GREP="Hi"
 HITI_DEVICE=$(lsusb |grep $HITI_GREP |grep -Eo 'Device ([0-9]{1,3})' |grep -Eo '[0-9]{1,3}')
 HITI_BUS=$(lsusb |grep $HITI_GREP |grep -Eo 'Bus ([0-9]{1,3})' |grep -Eo '[0-9]{1,3}')
-/media/$HITI_HD/usbreset /dev/bus/usb/$HITI_BUS/$HITI_DEVICE
+$HITI_HD/usbreset /dev/bus/usb/$HITI_BUS/$HITI_DEVICE
 lpr -o ppi=300 -o document-format=image/jpeg $1
 
