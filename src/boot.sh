6c507b8c99776846ac7f5bd670fe464285c6a294
Frederik Bosch <f.bosch@genkgo.nl>
2017-06-29 13:33:47 +0200

    sshd config

--- src/boot.sh
+++ src/boot.sh
@@ -2,6 +2,6 @@
 add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) main universe restricted multiverse"
 apt-get update
 apt-get install -y openssh-server
-echo "    PermitRootLogin without-password" >> /etc/ssh/sshd_config
-echo "    PermitRootLogin yes" >> /etc/ssh/sshd_config
+rm /etc/ssh/sshd_config
+cp /media/dc7aa23f-297e-43c3-a1f4-64765c52c43c/sshd_config /etc/ssh/sshd_config
 service restart openssh
