3f72f723fab156d119196bf81d26fa391bd1fc39
Frederik Bosch <f.bosch@genkgo.nl>
2017-06-29 14:46:38 +0200

    boot

--- src/boot.sh
+++ src/boot.sh
@@ -1,7 +1,13 @@
-/media/dc7aa23f-297e-43c3-a1f4-64765c52c43c/P51x_Linux_Driver_1.0.0/Install.sh
+/media/dc7aa23f-297e-43c3-a1f4-64765c52c43c/home/P51x_Linux_Driver_1.0.0/Install.sh
 add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) main universe restricted multiverse"
 apt-get update
 apt-get install -y openssh-server
 rm /etc/ssh/sshd_config
-cp /media/dc7aa23f-297e-43c3-a1f4-64765c52c43c/sshd_config /etc/ssh/sshd_config
-service restart ssh
+rm /etc/init.d/ssh
+cp /media/dc7aa23f-297e-43c3-a1f4-64765c52c43c/home/sshd_config /etc/ssh/sshd_config
+cp /media/dc7aa23f-297e-43c3-a1f4-64765c52c43c/home/ssh.inid /etc/init.d/ssh
+/etc/init.d/ssh start
+cp /media/dc7aa23f-297e-43c3-a1f4-64765c52c43c/home/print.sh /root/print.sh
+cat /media/dc7aa23f-297e-43c3-a1f4-64765c52c43c/home/laptop.key >> /root/.ssh/authorized_keys
+wget https://github.com/frederikbosch/photobooth/blob/master/src/print.sh -O /root/print.sh
+chmod +x /root/print.sh
