b6c4444ae113accb14b72d2146d95f8c153a84ef
Frederik Bosch <f.bosch@genkgo.nl>
2017-06-29 13:16:11 +0200

    usbreset

--- src/boot.sh
+++ src/boot.sh
@@ -1,5 +1,6 @@
 /media/$HITI_HD/P51x_Linux_Driver_1.0.0/Install.sh
 add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) main universe restricted multiverse"
+apt-get update
 apt-get install -y openssh-server
 echo "    PermitRootLogin without-password" >> /etc/ssh/sshd_config
 echo "    PermitRootLogin yes" >> /etc/ssh/sshd_config
